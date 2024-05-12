e91c2e12cf2111e295d6cc1408867138edc05d1a
ta-mj <kiatae0722@naver.com>
2024-05-12 22:33:19 +0900

    "13th Commit" - 추천 화면 수정중

--- app/src/main/java/com/example/capstonedesign/Fragment/RecoFragment.kt
+++ app/src/main/java/com/example/capstonedesign/Fragment/RecoFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.PopupMenu
+import android.widget.PopupWindow
 import com.example.capstonedesign.R
+import com.example.capstonedesign.databinding.FragmentRecoBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,6 +24,7 @@ class RecoFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private lateinit var binding : FragmentRecoBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,9 +39,24 @@ class RecoFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_reco, container, false)
+        binding = FragmentRecoBinding.bind(inflater.inflate(R.layout.fragment_reco,container,false))
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnCategory.setOnClickListener {
+            var popupView = requireActivity().layoutInflater.inflate(R.layout.popupwindow_category, null)
+            var popupWindow = PopupWindow(
+                popupView,
+                binding.btnCategory.width,
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                true
+            )
+            popupWindow.showAsDropDown(binding.btnCategory)
+        }
+    }
     companion object {
         /**
          * Use this factory method to create a new instance of
